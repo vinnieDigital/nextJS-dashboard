7549c639160b62e292d7b0fa64f9cdd11971c134
VinnieDigital <vinnie@Vinnies-MacBook-Pro-2017.local>
2024-07-09 16:41:52 -0400

    Merge Chapter 15 to Main and remove extra login button

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -68,7 +68,6 @@ export default function LoginForm() {
         <Button className="mt-4 w-full" aria-disabled={isPending}>
           Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
         </Button>
-        <LoginButton />
         <div className="flex h-8 items-end space-x-1">
           {errorMessage && (
             <>
